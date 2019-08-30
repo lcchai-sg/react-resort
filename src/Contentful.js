c21472e30c53d0e35d545b38a1a99bdea6c1bb91
lcchai-sg <lc.chai.sg@gmail.com>
2019-08-30 15:09:29 +0800

    using contentful

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,6 +1,6 @@
-const contentful = require("contentful");
+import { createClient } from 'contentful';
 
-export default contentful.createClient({
+export default createClient({
   space: process.env.REACT_APP_API_SPACE,
   accessToken: process.env.REACT_APP_ACCESS_TOKEN
 });
--- src/context.js
+++ src/context.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import items from './data';
+import Client from './Contentful';
 
 const RoomContext = React.createContext();
 
@@ -8,23 +9,100 @@ class RoomProvider extends Component {
     rooms: [],
     sortedRooms: [],
     featuredRooms: [],
-    loading: true
+    loading: true,
+    type: 'all',
+    capacity: 1,
+    price: 0,
+    minPrice: 0,
+    maxPrice: 0,
+    minSize: 0,
+    maxSize: 0,
+    breakfast: false,
+    pets: false,
   };
 
   // getData
+  getData = async () => {
+    try {
+      let result = await Client.getEntries({
+        content_type: 'resort',
+        order: '-fields.price'
+      });
+      let rooms = this.formatData(result.items);
+      let featuredRooms = rooms.filter(room => room.featured === true);
+      let maxPrice = Math.max(...rooms.map(item => item.price));
+      let maxSize = Math.max(...rooms.map(item => item.size));
+
+      this.setState({
+        rooms,
+        sortedRooms: rooms,
+        featuredRooms,
+        loading: false,
+        price: maxPrice,
+        maxPrice,
+        maxSize,
+      });
+
+    } catch (err) {
+      console.log('getData error: ', err);
+    }
+  }
 
   componentDidMount() {
-    // this.getdata();
-    let rooms = this.formatData(items);
-    let featuredRooms = rooms.filter(room => room.featured === true);
-
-    this.setState({
-      rooms,
-      sortedRooms: rooms,
-      featuredRooms,
-      loading: false
-    });
+    this.getData();
   }
+
+  handleChange = e => {
+    const target = e.target;
+    const name = target.name;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: value
+    }, this.filterRooms);
+  };
+
+  filterRooms = () => {
+    let {
+      rooms, type, capacity, price, minSize, maxSize, breakfast, pets
+    } = this.state;
+    // all the rooms
+    let tempRooms = [...rooms];
+
+    // transform value
+    capacity = parseInt(capacity);
+    price = parseInt(price);
+
+    // filter by type
+    if (type !== 'all') {
+      tempRooms = tempRooms.filter(room => room.type === type);
+    }
+
+    // filter by capacity
+    if (capacity !== 1) {
+      tempRooms = tempRooms.filter(room => room.capacity >= capacity);
+    }
+
+    // filter by price
+    tempRooms = tempRooms.filter(room => room.price <= price);
+
+    // filter by room size
+    tempRooms = tempRooms.filter(room => room.size >= minSize && room.size <= maxSize);
+
+    // filter by breakfast
+    if (breakfast) {
+      tempRooms = tempRooms.filter(room => room.breakfast === true);
+    }
+
+    // filter by pets
+    if (pets) {
+      tempRooms = tempRooms.filter(room => room.pets === true);
+    }
+
+    // set the state
+    this.setState({
+      sortedRooms: tempRooms,
+    })
+  };
 
   formatData(items) {
     let tempItems = items.map(item => {
@@ -45,7 +123,11 @@ class RoomProvider extends Component {
 
   render() {
     return (
-      <RoomContext.Provider value={{ ...this.state, getRoom: this.getRoom }}>
+      <RoomContext.Provider value={{
+        ...this.state,
+        getRoom: this.getRoom,
+        handleChange: this.handleChange,
+      }}>
         {this.props.children}
       </RoomContext.Provider>
     );
@@ -53,5 +135,13 @@ class RoomProvider extends Component {
 }
 
 const RoomConsumer = RoomContext.Consumer;
+
+export function withRoomConsumer(Component) {
+  return function ConsumerWrapper(props) {
+    return <RoomConsumer>
+      {value => <Component {...props} context={value} />}
+    </RoomConsumer>
+  }
+}
 
 export { RoomProvider, RoomConsumer, RoomContext };
